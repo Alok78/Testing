c2ec40891b11dab5ec73b5d2d71f7fa6a45b2653
Alok78 <alokkaushalrbl@gmail.com>
2023-01-17 15:25:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,14 @@
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+
 public class HelloWorld
 {
 	public static void main(String[] args)
 	{
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+  		LocalDateTime now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));  
+		
 		System.out.println("Welcome To Jenkins Testing ");
 	}
 }
